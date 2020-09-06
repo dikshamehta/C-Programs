fc66a248f1841c0aa84cc389e5a932b6a14fdd6d
dikshamehta <500061597@stu.upes.ac.in>
2020-09-06 08:06:25 +0530

    doubly linked list-implemented delete

--- DataStructures/doubly_linked_list.c
+++ DataStructures/doubly_linked_list.c
@@ -47,7 +47,7 @@ NODE* insert_before(NODE *node, int data){
 
 //===========================insert after the given node==========================
 
-void insert_after(NODE* node, int data){
+NODE *insert_after(NODE* node, int data){
     NODE *new_node = (NODE*)malloc(sizeof(NODE));
     new_node->data = data;  
     new_node->next = NULL;
@@ -69,6 +69,30 @@ void insert_after(NODE* node, int data){
         new_node->prev = node;
 
     }
+    return new_node;
+}
+
+//=======================delete the node whose address is given======================
+
+void delete(NODE *node){
+    if(node == NULL){
+        printf("enter valid element");
+        return;
+    }
+
+    //checking links next to node 
+    if(node->next!=NULL){
+        node->next->prev = node->prev;
+    }//else do nothing
+
+    //checking links previous of node 
+    if(node->prev == NULL){
+        head = head->next;
+    }
+    else{
+        node->prev->next = node->next;
+    }
+    free(node);
 }
 
 
@@ -90,13 +114,18 @@ void traverse(){
 int main(){
     
     NODE *new_node = insert_before(head, 4);
-    //new_node = insert_before(new_node, 3);
-    //new_node = insert_before(new_node, 1);
+    new_node = insert_before(new_node, 3);
+    new_node = insert_before(new_node, 1);
 
     traverse();
 
-    insert_after(new_node, 8);
+    new_node = insert_after(new_node, 8);
 
     traverse();
+
+    delete(new_node);
+
+    traverse();
+
     return 0;
 }
