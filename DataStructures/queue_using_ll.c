d972e11c3475e56aec18a43bc4d5bbe642c83301
dikshamehta <500061597@stu.upes.ac.in>
2020-09-08 10:39:00 +0530

    implemented stack using queue

--- DataStructures/queue_using_ll.c
+++ DataStructures/queue_using_ll.c
@@ -9,51 +9,77 @@ struct Node{
 
 typedef struct Node NODE;
 
-NODE *front = NULL;
-NODE *rear = NULL;
+//making structure of front and rear
+struct Queue{
+    NODE *front;
+    NODE *rear;
+};
 
-void enqueue(int data){
+typedef struct Queue QUEUE;
+
+// NODE *front = NULL;
+// NODE *rear = NULL;
+
+
+void enqueue(QUEUE *q, int data){
+    //here, q = is a pointer to a variable of QUEUE type, 
+    //i.e. QUEUE structure's address is passed and through that we will acces front and rear
+    
+    //&(q->front) this means address of front variable store inside queue and similarly with rear 
+    //also here we are taking **front as the value of front pointer of struct can change in this function
+    NODE **front = &(q->front);
+    NODE **rear = &(q->rear);
+
     NODE *new_node = (NODE*)malloc(sizeof(NODE));
     new_node -> data = data;
     new_node -> next = NULL;
     new_node -> prev = NULL;
 
-    if(rear == NULL){
-        rear = new_node;
-        front = new_node;
+    if(*rear == NULL){
+        *rear = new_node;
+        *front = new_node;
     }
     else{
-        NODE *temp = rear;
-        rear = new_node;
+        NODE *temp = *rear;
+        *rear = new_node;
         new_node->next = temp;
         temp->prev = new_node;
     }
 
 }
 
-int dequeue(){
-    if(rear == NULL){
+int dequeue(QUEUE *q){
+    NODE **front = &(q->front);
+    NODE **rear = &(q->rear);
+
+    if(*rear == NULL){
         printf("empty queue");
         return -1;
     }
-
     else{
-        NODE *temp = front;
-        int data = front->data; 
-        front->prev->next = NULL;
-        front = front->prev;
+        NODE *temp = *front;
+        int data = (*front)->data;
+        if(*rear == *front){
+            //only one element in the queue 
+            *rear = NULL;
+            *front = NULL; 
+        } 
+        else{
+            (*front)->prev->next = NULL;
+            *front = (*front)->prev;
+        }
         free(temp);
         return data;
     }
-
 }
 
-void traverse(){
-    if(rear == NULL){
-        printf("No element");
+void traverse(QUEUE *q){
+    NODE **rear = &(q->rear);
+    if(*rear == NULL){
+        printf("No element\n");
         return;
     }
-    NODE *t = rear; 
+    NODE *t = *rear; 
     while(t != NULL){
         printf("%d ", t->data);
         t = t->next;
@@ -61,23 +87,29 @@ void traverse(){
     printf("\n");
 }
 
-int main(){
-
-    enqueue(4);
-    enqueue(3);
-    enqueue(5);
-
-    traverse();
-
-    int data = dequeue();
-
-    printf("deleted %d \n", data);
-
-    traverse();
-
-    enqueue(6);
-
-    traverse();
-
-    return 0; 
+int isempty(QUEUE *q){
+    return (q->rear == NULL);
 }
+
+// int main(){
+//     QUEUE q1;
+//     q1.front = NULL;
+//     q1.rear = NULL; 
+//     enqueue(&q1, 4);
+//     enqueue(&q1, 3);
+//     enqueue(&q1, 5);
+
+//     traverse(&q1);
+
+//     dequeue(&q1);
+//     dequeue(&q1);
+//     dequeue(&q1);
+
+//     traverse(&q1);
+
+//     enqueue(&q1, 6);
+
+//     traverse(&q1);
+
+//     return 0; 
+// }
