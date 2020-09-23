5e6b08871fac30556a667984832ea4359429ba0b
dikshamehta <500061597@stu.upes.ac.in>
2020-09-23 07:38:48 +0530

    dynamic programming - lcs

--- Algorithms/dynamic_programming/lcs.c
+++ Algorithms/dynamic_programming/lcs.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<string.h>
+#include<stdlib.h>
 /*
 string : aabcdeffg
 subsequence : abde, dffg
@@ -14,18 +15,60 @@ int max(int x, int y){
 }
 
 //recursive method
-int lcs(char *s1, char *s2, int p1, int p2){
-    if(p1==-1 || p2==-1){
+int lcs(char *s1, char *s2, int l1, int l2){
+    if(l1==-1 || l2==-1){
         return 0;
     }
 
-    if(s1[p1]==s2[p2]){
-        return 1+lcs(s1, s2, p1-1, p2-1);
+    if(s1[l1]==s2[l2]){
+        return 1+lcs(s1, s2, l1-1, l2-1);
     }
     else{
-        int ls1 = lcs(s1, s2, p1-1, p2);
-        int ls2 = lcs(s1, s2, p1, p2-1);
+        int ls1 = lcs(s1, s2, l1-1, l2);
+        int ls2 = lcs(s1, s2, l1, l2-1);
         return max(ls1, ls2);
+    }
+}
+
+//here in recursion if we make tree structure still a lot of recursive calls are repeating so dynamic prog should be used
+
+/*
+array structure
+l1 = 3, l2 = 4
+table size = 4*5
+
+0   0   0   0   0
+0  -1  -1  -1  -1
+0  -1  -1  -1  -1
+0  -1  -1  -1  -1
+
+this is the structure that will form after code will run and -1 will be replaced wit
+respective values of lcs on that particular coordinate
+
+For eg lcs(1,1) will be stored at table[1][1] 
+*/
+
+
+//dynamic programming 
+int lcs_dp(int **table, char *s1, char *s2, int l1, int l2){
+    if(l1==0 || l2==0){
+        table[l1][l2] = 0;
+        return 0; 
+    }
+    if(s1[l1-1]==s2[l2-1]){
+        if(table[l1][l2]==-1){
+            table[l1][l2] = 1 + lcs_dp(table ,s1, s2, l1-1, l2-1);
+        }
+        return table[l1][l2];
+    }
+    else{
+        if(table[l1-1][l2]==-1){
+            table[l1-1][l2] = lcs(s1, s2, l1-1, l2);
+        }
+        else if(table[l1][l2-1]==-1){
+            table[l1][l2-1] = lcs(s1, s2, l1, l2-1);
+        }
+        return max(table[l1-1][l2], table[l1][l2-1]);
     }
 }
 
@@ -37,8 +80,23 @@ int main(){
     int l1 = strlen(s1);
     int l2 = strlen(s2);
 
-    int len = lcs(s1, s2, l1-1, l2-1);
+    int m = l1+1; //no. of rows 
+    int n = l2+1; //no. of columns 
 
+    int **table = (int**)malloc(sizeof(int*)*m);
+    for(int i=0; i<m; i++){
+        table[i] = (int*)malloc(sizeof(int)*n);
+    }
+
+    for(int i=0; i<m; i++){
+        for(int j=0; j<n; j++){
+            table[i][j] = -1;
+        }
+    }
+
+    //int len = lcs(s1, s2, l1-1, l2-1);
+
+    int len = lcs_dp((int **)table, s1, s2, l1, l2); //(int **) is type casting 
     printf("%d\n", len);
     return 0;
 }
