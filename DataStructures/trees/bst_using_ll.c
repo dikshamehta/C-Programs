8f79528a36d1146e554b21bc6691bf153c879309
dikshamehta <500061597@stu.upes.ac.in>
2020-09-25 09:43:01 +0530

    data structures-bst,insert iterative

--- DataStructures/trees/bst_using_ll.c
+++ DataStructures/trees/bst_using_ll.c
@@ -17,53 +17,68 @@ struct bst{
 
 typedef struct bst BST; 
 
-// void insert(int data){
-//     //creating first node 
-//     if(root == NULL){
-//         NODE r = (NODE*)malloc(sizeof(NODE));
-//         r.data = data;
-//         r->left = NULL;
-//         r->right = NULL;
-
-//         root = r;
-//         return;
-//     }
-
-//     NODE *current = root;
-//     while(current->right!=NULL || current->left!=NULL){
-//         if(data>current.data){
-//            current = current->right;
-//         }
-        
-//     }
-//}
-
-void insert_recur(BST *tree, NODE *parent, int data){
-    //creating a node
+void insert(BST *tree, int data){
+    //creating a node 
     NODE *r = (NODE*)malloc(sizeof(NODE));
     r->data = data;
     r->left = NULL;
     r->right = NULL;
-    
+
     //base case 
-    if(parent == NULL){
+    if(tree->root == NULL){
         tree->root = r;
         return;
     }
 
-    if((parent->data) >= data){
-        //left tree
-        if(parent->left==NULL){
-            parent->left = r;
+    NODE **current = &(tree->root);
+
+    /*
+    *p = value at address that p is storing 
+
+    current = 100
+
+    |100| (address of address of root node)
+    |
+    |200| (address of root node)
+
+    |200|
+    |
+    root node
+
+    now this root node is further storing left and right pointers which are storing their addresses further 
+
+    now we want to store the adress of right pointer of root into current 
+    */
+
+    while(*current != NULL){
+        if(data > (*current)->data){
+           current = &((*current)->right);
         }
-        else insert_recur(tree, parent->left, data);
+        else{
+            current = &((*current)->left);
+        }
+    }
+
+    *current = r;    
+    return;
+}
+
+void insert_recur(NODE **parent, int data){
+    //base case 
+    if(*parent == NULL){
+        //creating a node
+        NODE *r = (NODE*)malloc(sizeof(NODE));
+        r->data = data;
+        r->left = NULL;
+        r->right = NULL;
+
+        *parent = r;
+    }
+    else if(data > (*parent)->data){
+        insert_recur(&((*parent)->right), data);
     }
     else{
-        //right tree
-        if(parent->right==NULL){
-            parent->right = r;
-        }
-        else insert_recur(tree, parent->right, data);
+        insert_recur(&((*parent)->left), data);
     }
 }
 
@@ -82,10 +97,16 @@ int main(){
     BST tree;
     tree.root = NULL;
 
-    insert_recur(&tree, tree.root, 2);
-    insert_recur(&tree, tree.root, 3);
-    insert_recur(&tree, tree.root, 1);
+    insert_recur(&(tree.root), 2);
+    insert_recur(&(tree.root), 3);
+    insert_recur(&(tree.root), 1);
 
-    traversal_inorder(tree.root);
+    // insert(&tree, 2);
+    // insert(&tree, 3);
+    // insert(&tree, 1);
+    // insert(&tree, 4);
+ 
+
+    traversal_inorder(tree.root); //left root right 
     return 0;
 }
